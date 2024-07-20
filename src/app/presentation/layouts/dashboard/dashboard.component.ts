6921cf294bbf0e99ca62995c48605f60eaace640
Franco Cabrera <cabrera.franco@outlook.com>
2024-07-20 14:21:12 -0300

    initial commit

---
+++ src/app/presentation/layouts/dashboard/dashboard.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './dashboard.component.html',
+
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DashboardComponent {}
